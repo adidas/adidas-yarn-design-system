1dfffb72e38c95cfa9c344fa9b8496ae9ca7d325
Jaime Pastor <jaime.pastor@adidas-group.com>
2018-11-13 12:31:58 +0100

    Refactor services code

--- example/services/forms.js
+++ example/services/forms.js
@@ -1,5 +1,16 @@
+import { getComponents } from './components';
+
+/**
+ * Returns the list of tabs in the form page.
+ * @param {string} activeTabId - Id of the active tab.
+ * @returns {Object[]} List of tabs.
+ */
 export function getFormTabs(activeTabId) {
-  return [ 'input', 'checkbox', 'radio', 'textarea' ].map((tab) => ({
+  const formComponents = getComponents('forms');
+
+  console.log('formComponents', formComponents);
+
+  return formComponents.map((tab) => ({
     id: tab,
     to: `/components/form/${ tab }`,
     active: tab === activeTabId
