95353583a58870452a13a2f5916f30dbe8138a9b
Jaime Pastor <jaime.pastor@adidas-group.com>
2018-11-26 13:52:13 +0100

    Fix tags and add example

---
+++ example/components/tag/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="tag" :class="{ 'tag--selected': selected }">
+    <span class="tag__label">{{ label }}</span>
+    <button class="tag__icon tag__icon--remove yarn-icon yarn-icon--close"
+        v-if="removable">
+    </button>
+  </span>
+</template>
+
+<script>
+export default {
+  props: [ 'label', 'removable', 'selected' ]
+};
+</script>
---
+++ example/pages/components/tags.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <page-title>
+      <p>{{ $t('views.components.name') }}</p>
+      <h2>{{ $t('views.components-tags.name') }}</h2>
+    </page-title>
+    <div class="row">
+      <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+        <section class="section">
+          <h5>{{ $t('views.components-tags.horizontal') }}</h5>
+          <span class="tag-list tag-list--horizontal">
+            <tag v-for="tag in tags"
+                :key="tag"
+                :label="tag">
+            </tag>
+          </span>
+        </section>
+        <section class="section">
+          <h5>{{ $t('views.components-tags.vertical') }}</h5>
+          <span class="tag-list tag-list--vertical">
+            <tag v-for="tag in tags"
+                :key="tag"
+                :label="tag">
+            </tag>
+          </span>
+        </section>
+        <section class="section">
+          <h5>{{ $t('views.components-tags.selected') }}</h5>
+          <span class="tag-list tag-list--horizontal">
+            <tag v-for="tag in tags"
+                :key="tag"
+                :label="tag"
+                :selected="true">
+            </tag>
+          </span>
+        </section>
+        <section class="section">
+          <h5>{{ $t('views.components-tags.removable') }}</h5>
+          <span class="tag-list tag-list--horizontal">
+            <tag v-for="tag in tags"
+                :key="tag"
+                :label="tag"
+                :removable="true">
+            </tag>
+          </span>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tag from '~/components/tag/default';
+
+export default {
+  data() {
+    return {
+      tags: [ 'size: 8-', 'color: scarlet', 'sport: running', 'technology: boost' ]
+    };
+  },
+  components: {
+    Tag
+  }
+}
+</script>
