f70c039252c9ce556e98c9b1308806ea9cb0a118
Jaime Pastor <jaime.pastor@adidas-group.com>
2018-11-26 15:26:55 +0100

    Fix tiles and add example

---
+++ example/components/tile/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tile">
+    <div class="tile__main" :style="{ backgroundImage: `url(${ image })` }">
+      <div class="tile__main--type" v-if="type">
+        {{ type }}
+      </div>
+      <div class="tile__main--flag" v-if="flag">
+        <span :class="`yarn-icon yarn-icon--${ flag }`"></span>
+      </div>
+    </div>
+    <div class="tile__footer">
+      <div class="tile__footer__group">{{ title }}</div>
+      <div class="tile__footer__subheader">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: '',
+    image: '',
+    type: '',
+    flag: ''
+  }
+};
+</script>
